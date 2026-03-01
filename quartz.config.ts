2d9e17519d44fbb63497ee0be0086d213d895fe8
seamanship <thelegacyofpants@gmail.com>
2026-02-28 20:45:08 -0800

    Quartz sync: Feb 28, 2026, 8:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -89,7 +89,13 @@ const config: QuartzConfig = {
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+                Plugin.CustomOgImages({
+                colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
+                width: 1200, // width to generate with (in pixels)
+                height: 630, // height to generate with (in pixels)
+                excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
+                imageStructure: defaultImage, // custom image component to use
+            }),,
     ],
   },
 }
