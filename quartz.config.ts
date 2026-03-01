9df64f8ff720ceee7b5ad594ac15b899fe4409b5
seamanship <thelegacyofpants@gmail.com>
2026-02-28 19:49:01 -0800

    Quartz sync: Feb 28, 2026, 7:49 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "notes on seamanship",
-    pageTitleSuffix: "",
+    pageTitle: "seamanship library",
+    pageTitleSuffix: "₊˚⊹ ㅅ ⚓︎",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -33,20 +33,20 @@ const config: QuartzConfig = {
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
+          dark: "#040411",
+          secondary: "#008b58",
+          tertiary: "#5ec8c8",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#040411",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#289141",
-          tertiary: "#84a59d",
+          secondary: "#008b58",
+          tertiary: "#5ec8c8",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },
