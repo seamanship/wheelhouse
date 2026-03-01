628ff67ed04bb620c10a5cdb7b749c42264c87a0
seamanship <thelegacyofpants@gmail.com>
2026-02-28 19:51:05 -0800

    Quartz sync: Feb 28, 2026, 7:51 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "⚓︎ Main Website": "https://safe.voyage/",
     },
   }),
 }
